49159777da8d88613c681b12b5283b7bc4746906
Tristan Bietsch <tristanmbietsch@gmail.com>
2021-06-19 19:42:43 -0500

    finished game #2

--- games.py
+++ games.py
@@ -2,12 +2,12 @@ import random
 
 money = 100
 
-#Write your game of chance functions here
+print('-----------------------------------------------')
+# COIN FLIP GAME
 def flip_coin(guess, bet):
     global money
     if money > 0:
         if bet < money:
-            payout = 0
             print("It is time to flip a coin!")
             print("You bet " + str(bet) + " and credits on " + str(guess) + ".")
             #Random integer to represent heads / tails
@@ -34,11 +34,55 @@ def flip_coin(guess, bet):
             print("Bet cannot exceed your balance")
             print("Please lower your bet")
             print("Bet must be less than " + str(money) + " credits")
-        
+            
         print("Thanks for playing!")
         print("-------------------------------------")
     else:
-         print("Honestly you should probably stop playing; it seems either your luck has run out, or you have a gambling problem.")
+         print('You have no money :(')
+
+#  CHO - HAN GAME
+def cho_han(guess, bet):
+  global money
+  if bet < money:
+    payout = 0
+    print("Let's play Cho-Han!")
+    print("In this game, you must guess the sum of two dice rolls is even or odd.")
+    print("You guessed: " + str(str.upper(guess)) + " and bet " + str(bet) + " credits.")
+    #Roll dice and find the total
+    die_1 = random.randint(1,6)
+    die_2 = random.randint(1,6)
+    total = die_1 + die_2
+    print("Rolling die......")
+    print("Die 1: " + str(die_1))
+    print("Die 2: " + str(die_2))
+    print("Sum of roll: " + str(total))
+    #Assign odd/even value to die roll (mod 2 being 0 means even number)
+    if total % 2 > 0:
+      result = "odd"
+    else:
+      result = "even"
+    #Did the player win?
+    if str.lower(guess) == result:
+      print("You win! " + str(bet) + " credits have been added to your account!")
+      money += bet
+      print('Processing transaction...')
+      print("Your new account balance is: " + str(money) + " credits.")
+    else:
+      print("You lose! " + str(bet) + " credits have been subtracted from your account!")
+      payout += -1 * bet
+      money += payout
+      print('Processing transaction...')
+      print("Your new account balance is: " + str(money) + " credits.")
+  else:
+    print("Bet cannot exceed account balance!")
+    print("Please lower your bet.")
+    print("Bet must be less than " + str(money) + " credits.")
+  print("Thanks for playing!")
+  print("------------------------------")  
+
+
 #Call your game of chance functions here
 
 flip_coin('Heads', 10)
+cho_han('odd', 15)
+
